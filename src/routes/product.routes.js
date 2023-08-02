d206f41ac7504071eff4dc7bf2659e303bf8a65f
avinash singh rathore <avinashrathore1.a@gmail.com>
2023-08-02 23:14:46 +0530

    delete product component done

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,22 +3,15 @@ const productController = require("../controller/product.controller");
 
 const router = express.Router();
 
-// router
-//   .get('/', studentController.getAllStudentDetails)
-//   .get('/:id', studentController.getStudentDetails)
-//   .put('/:id', studentController.replaceStudentDetails)
-//   .patch('/:id', studentController.updateStudentDetails)
-//   .delete('/:id', studentController.deleteStudentDetails);
-// exports.router = router;
-
 // crud routes
 router.post("/add-product", productController.addNewProduct);
-router.get("/", productController.getAllProductDetails);
+router.get("/get-all-products", productController.getAllProductDetails);
 // router.get("/:id", productController.getProductDetails);
 
 // // Protected Routes
 // router.put("/:id", userController.replaceUserDetails);
 // router.patch("/:id", userController.updateUserDetails);
-// router.delete("/:id", userController.deleteUserDetails);
+router.delete("/delete-product/:id", productController.deleteProductDetails);
 
 exports.router = router;
+
